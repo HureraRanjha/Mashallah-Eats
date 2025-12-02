1a38000959a60cd32f98db33e136de487529829a
Your Name <ahuossein20@gmail.com>
2025-12-02 01:30:30 -0500

    fixed supabase, added templates, added MenuItem table and implemented browse view

--- myapp/api/urls.py
+++ myapp/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, DishListView
 
 urlpatterns = [
     path("index/", index),   # → /api/index/
+    path("browse/", DishListView, name="dishes-list")
 ]
--- myapp/api/views.py
+++ myapp/api/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .supabase_client import supabase
 def index(request):
     return HttpResponse("Hello from the backend API")
+
+
+def DishListView(request):
+    response = supabase.table("Menuitem").select("*").execute()
+    items = response.data
+    print("TABLE TEST:", supabase.table("Menuitem").select("*").execute())
+    return render(request, 'browse.html', {"items": items})
