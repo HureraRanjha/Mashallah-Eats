53e9caebff6a2253c9ba51eed43ad71cdbfa92d1
Your Name <ahuossein20@gmail.com>
2025-12-03 01:42:11 -0500

    fixed supabase issues and added login feature

--- myapp/api/views.py
+++ myapp/api/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .supabase_client import supabase
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth.views import LoginView
+from .models import MenuItem
 def index(request):
     return HttpResponse("Hello from the backend API")
 
 
 def DishListView(request):
-    response = supabase.table("Menuitem").select("*").execute()
-    items = response.data
-    print("TABLE TEST:", supabase.table("Menuitem").select("*").execute())
+    items = MenuItem.objects.all()   # <-- Django ORM now
     return render(request, 'browse.html', {"items": items})
+
+
+class LoginUser(LoginView):
+    template_name = "login.html"
