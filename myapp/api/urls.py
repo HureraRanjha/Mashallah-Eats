4e1e6f36f6f8d49750ed12bb26dd19ca23c49287
Hurera Ranjha <hureraranjha9@gmail.com>
2025-12-06 22:33:12 -0500

    Created Submit Bid, Get Bid, Assign Bid

--- myapp/api/urls.py
+++ myapp/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, DishListView, LoginUser, Discussions, create_reply, create_topic, order_food, food_review, add_menu
+from .views import index, DishListView, LoginUser, Discussions, create_reply, create_topic, order_food, food_review, add_menu, create_delivery_bid, get_delivery_bids, assign_delivery
 
 
 urlpatterns = [
@@ -12,5 +12,8 @@ urlpatterns = [
     path("topic/", create_topic, name="topic"),
     path("order/", order_food, name="order"),
     path("review_food/", food_review, name="review_food"),
-    path("add_item/", add_menu, name="add_item")
+    path("add_item/", add_menu, name="add_item"),
+    path("bid/", create_delivery_bid, name="create_bid"),
+    path("bids/", get_delivery_bids, name="get_bids"),
+    path("assign_delivery/", assign_delivery, name="assign_delivery"),
 ]
