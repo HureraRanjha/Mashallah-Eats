d37a86a096e26788586dca467dd583db52d35fc3
Hurera Ranjha <hureraranjha9@gmail.com>
2025-12-07 00:56:47 -0500

    Made stripe endpoints

--- myapp/api/urls.py
+++ myapp/api/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import index, DishListView, LoginUser, Discussions, create_reply, create_topic, order_food, food_review, add_menu, create_delivery_bid, get_delivery_bids, assign_delivery, delivery_rating, RegisterUser
+from .views import index, DishListView, LoginUser, Discussions, create_reply, create_topic, order_food, food_review, add_menu, create_delivery_bid, get_delivery_bids, assign_delivery, delivery_rating, RegisterUser, create_deposit_intent, confirm_deposit
 
 urlpatterns = [
     path("index/", index),   # → /api/index/
     path("browse/", DishListView, name="dishes-list"),
     path("login/", LoginUser.as_view(), name="login"),
     path("register/", RegisterUser, name="register"),
-    path("register/", LoginUser.as_view(), name="register"),
     path("discussion_board/", Discussions, name="discussion_board"),
     path("reply/", create_reply, name="reply"),
     path("topic/", create_topic, name="topic"),
@@ -16,6 +15,7 @@ urlpatterns = [
     path("bid/", create_delivery_bid, name="create_bid"),
     path("bids/", get_delivery_bids, name="get_bids"),
     path("assign_delivery/", assign_delivery, name="assign_delivery"),
-    path("review_driver/", delivery_rating, name="review_Driver"),
-
+    path("review_driver/", delivery_rating, name="review_driver"),
+    path("deposit/create/", create_deposit_intent, name="create_deposit"),
+    path("deposit/confirm/", confirm_deposit, name="confirm_deposit"),
 ]
