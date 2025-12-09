883e96ca5a9493af667cffeea367b5da218275ed
Hurera Ranjha <hureraranjha9@gmail.com>
2025-12-09 00:17:36 -0500

    Fixed Checkout Bug, CORS error

--- myapp/backend/settings.py
+++ myapp/backend/settings.py
@@ -62,7 +62,11 @@ ROOT_URLCONF = 'backend.urls'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:3001",
+    "http://127.0.0.1:3001",
 ]
+
+CORS_ALLOW_CREDENTIALS = True
 
 
 TEMPLATES = [
