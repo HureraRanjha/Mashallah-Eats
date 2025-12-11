d2b631e0f77ace1579da3f8ecc8de4030fc05d2e
Hurera Ranjha <hureraranjha9@gmail.com>
2025-12-11 05:23:28 -0500

    Bigger Report Icon

--- frontend/src/pages/DiscussionPost.js
+++ frontend/src/pages/DiscussionPost.js
@@ -179,10 +179,10 @@ export default function DiscussionPost() {
                       {canReport && comment.author && comment.author !== currentUsername && (
                         <Link
                           to={getReportUrl(comment.author, `Comment on "${post.title}":\n${comment.body}`)}
-                          className="btn btn-ghost btn-xs text-error"
+                          className="btn btn-ghost btn-sm text-error"
                           title="Report this comment"
                         >
-                          <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 21v-4m0 0V5a2 2 0 012-2h6.5l1 1H21l-3 6 3 6h-8.5l-1-1H5a2 2 0 00-2 2zm9-13.5V9" />
                           </svg>
                         </Link>
